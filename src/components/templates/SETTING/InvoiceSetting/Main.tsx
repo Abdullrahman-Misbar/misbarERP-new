ddfdfa3e77230e147be320c753a25c17bf2c9dad
Nasohy Ebrahim <Nasohy.ebrahim@misbar-tech.com>
2024-12-12 14:55:34 +0200

    complete setting checkbox and radio button

--- src/components/templates/SETTING/InvoiceSetting/Main.tsx
+++ src/components/templates/SETTING/InvoiceSetting/Main.tsx
@@ -1,98 +1,45 @@
-import { Grid } from "@mui/material";
-import RadioButtons from "../../../atoms/formik/RadioComp";
-import CloseOrder from "./closeOrder";
-import Invoices from "./Invoices";
+import { Form, Formik } from "formik";
+import MainData from "./MainData";
 
 function Main() {
+  const initialValues = {
+    settings: [
+      {
+        id: 16,
+        settingId: 16,
+        isDisabled: true,
+        settingKey: "exceedAllowedValueWarning",
+        settingValue: "",
+      },
+      {
+        id: 17,
+        settingId: 17,
+        isDisabled: true,
+        settingKey: "exceedAllowedValueWarning",
+        settingValue: "",
+      },
+      {
+        id: 18,
+        settingId: 18,
+        isDisabled: true,
+        settingKey: "exceedAllowedValueWarning",
+        settingValue: "",
+      },
+      {
+        id: 19,
+        settingId: 19,
+        isDisabled: true,
+        settingKey: "exceedAllowedValueWarning",
+        settingValue: "",
+      },
+    ],
+  };
   return (
-    <>
-      <div className="bg-white rounded-lg  p-5">
-        <Invoices />
-      </div>
-
-      <div className="!my-5 w-full">
-        <Grid container spacing={4}>
-          {/* Left Column */}
-          <Grid item xs={12} sm={6}>
-            <div className="bg-white rounded-lg p-6 my-4">
-              <span className="text-xl font-semibold">سياسة الفوترة</span>
-              <div className="flex items-center gap-4 mt-6 flex-wrap">
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="الكميات المطلوبة"
-                  onChange={() => {}}
-                />
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="الكميات المستلمة"
-                  onChange={() => {}}
-                />
-              </div>
-            </div>
-
-            <div className="bg-white rounded-lg p-6 my-4">
-              <span className="text-xl font-semibold">الكميات المجانية</span>
-              <div className="mt-6">
-                <Grid container spacing={2} alignItems="center">
-                  <Grid item xs={12} sm={6}>
-                    <RadioButtons
-                      name="priceIncludeTax"
-                      label="لاتؤثر على الفاتورة"
-                      onChange={() => {}}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <RadioButtons
-                      name="priceIncludeTax"
-                      label="ايراد للشركة"
-                      onChange={() => {}}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <RadioButtons
-                      name="priceIncludeTax"
-                      label="تخفض تكلفة الأصناف"
-                      onChange={() => {}}
-                    />
-                  </Grid>
-                </Grid>
-              </div>
-            </div>
-          </Grid>
-
-          {/* Right Column */}
-          <Grid item xs={12} sm={6}>
-            <div className="bg-white w-full rounded-lg p-6 pb-16 my-4">
-              <span className="text-xl font-semibold">مصاريف الشراء</span>
-              <div className="mt-6 flex flex-col gap-4">
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="توزع على الأصناف نسبة وتناسب حسب القيمة"
-                  onChange={() => {}}
-                />
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="توزع على الأصناف يدويا"
-                  onChange={() => {}}
-                />
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="توزع على الأصناف بالتساوي"
-                  onChange={() => {}}
-                />
-                <RadioButtons
-                  name="priceIncludeTax"
-                  label="لا تؤثر على التكلفة"
-                  onChange={() => {}}
-                />
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
-      {/* Close orders  */}
-      <CloseOrder />
-    </>
+    <Formik onSubmit={() => {}} initialValues={initialValues}>
+      <Form>
+        <MainData />
+      </Form>
+    </Formik>
   );
 }
 export default Main;
--- src/components/templates/SETTING/OrderSetting/CloseOrder.tsx
+++ src/components/templates/SETTING/OrderSetting/CloseOrder.tsx
@@ -1,13 +1,28 @@
+import { useFormikContext } from "formik";
 import { SwitchComp } from "../../../atoms/formik/SwitchComp";
 
 function CloseOrder() {
+  const { values, setFieldValue } = useFormikContext();
+
+  const handleSwitchChange = (id: number, checked: boolean) => {
+    const updatedSettings = values?.settings?.map((item) =>
+      item.id === id ? { ...item, isDisabled: checked } : item
+    );
+    setFieldValue("settings", updatedSettings);
+    
+  };
+
   return (
     <>
       <span className="text-xl font-bold">اقفال الأوامر</span>
       <div className="flex items-center justify-center gap-12 w-full ">
         <div className="flex flex-col gap-12 w-[50%]">
           <div className="flex items-center justify-start gap-3 ">
-            <SwitchComp name="" />
+            <SwitchComp
+              name="approveOrderBelowValue"
+              defaultChecked={!values?.settings[13]?.isDisabled}
+              onChange={(e) => handleSwitchChange(14, !e.target.checked)}
+            />
             <div className="text-right">
               <p className="m-1"> المكتملة </p>
               <p className="font-[12px] text-light font-somarLight my-1">
@@ -20,7 +35,11 @@ function CloseOrder() {
 
         <div className="flex flex-col gap-12 w-[50%]">
           <div className="flex items-center justify-start gap-3 ">
-            <SwitchComp name="" />
+            <SwitchComp
+              name="approveOrderBelowValue"
+              defaultChecked={!values?.settings[14]?.isDisabled}
+              onChange={(e) => handleSwitchChange(15, !e.target.checked)}
+            />
             <div className="text-right">
               <p className="m-1"> المكتملة </p>
               <p className="font-[12px] text-light font-somarLight my-1">
